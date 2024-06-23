4d1231972b2b15facd922ec0a1ed0ad43671bbf8
Sanjeevi Kumar <sanjeevik89@gmail.com>
2024-06-23 23:34:48 +0530

    fix web scraping

--- index.js
+++ index.js
@@ -4,10 +4,21 @@
 let url = "https://www.goodreturns.in/petrol-price-in-pudukkottai.html";
 let req = new Request(url);
 let res = await req.loadString();
-let price = res.match(/\<title\>(.*?)\<\/title\>/)[1].replace('<title>', '').replace('<\title>', '').replace('Petrol Price in Pudukkottai, Petrol Rate Today', '').replace(' - Goodreturns','').match(/Rs.(.*?)\/Ltr/)[1].trim();
-console.log(price);
-let change = res.match(/\<div class="fuel-block-details"\>(.*?)\<i  class=""\>/s)[1].replace(/&#8377;(.*?)\( &#8377;/s, "").trim();
-console.log(change);
+ // Extracting petrol in pudukkottai price
+ const priceMatch = res.match(/<title>(.*?)<\/title>/); 
+ const price = priceMatch[1].replace('Petrol Price in Pudukkottai, Petrol Rate Today', '').replace(' - Goodreturns', '').match(/Rs.(.*?)\/Ltr/)[1].trim();
+ console.log(`Petrol price: ₹${price}/Ltr`);
+ fuelDetailsBlock = res.match(/\<div class="fuel-block-details"\>(.*?)\<\/div>/s)[1];        
+ fuelChangeBlock = fuelDetailsBlock.match(/\<span class="(.*?)"\>(.*?)\<i class="/s)[2];        
+ // Extract the number using a regular expression
+ const match = fuelChangeBlock.match(/(\d+\.\d+)/);
+
+ if (match) {
+     const change = parseFloat(match[1]);            
+     console.log(`Change details: ${change}`); // Example: 0.20
+ } else {
+     console.log('Change details: NaN');
+ }
 let w = new ListWidget();
 let title = w.addText("Petrol PDKT:");
 
